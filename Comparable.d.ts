b1c0ba291b6f52891b3927f78c9cbaff01bcae4d
Mark Fulton <mark.fulton@asurion.com>
2023-07-03 08:29:41 -0500

    docs: Comparable

--- Comparable.d.ts
+++ Comparable.d.ts
@@ -1,6 +1,9 @@
 import $ from "💰/$.ts";
 import "💰/$/compareTo.ts";
 
+/**
+ * Classes that implement this interface have a defined total ordering between their instances.
+ */
 export default interface Comparable<T extends Comparable<T>> {
   /**
    * Compares this value to another value for order (returns zero for same, negative for before, positive for after, and `NaN` for indeterminate).
