fbff2d165d19a3b864f8d5d53209329d24d02682
Mark Fulton <mfulton26@gmail.com>
2024-01-07 11:23:11 -0600

    chore: format from deno upgrade

--- BigInt/prototype/$.pow.ts
+++ BigInt/prototype/$.pow.ts
@@ -11,7 +11,7 @@ declare global {
 Object[$.defineDataProperty](
   BigInt.prototype,
   $.pow,
-  function(this: bigint, other) {
+  function (this: bigint, other) {
     return this ** other;
   },
 );
